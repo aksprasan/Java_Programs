a752eda279db4552148943a94f57ee7e2a575b44
Akshat Prasanna <149640760+aksprasan@users.noreply.github.com>
2025-08-24 00:55:51 -0400

    commiting beginning code

--- Beginning_Codes/FeetToMiles.java
+++ Beginning_Codes/FeetToMiles.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class FeetToMiles{
     public static void main(String[] args){
+        int feetToMile = 5280;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of feet: ");
         int num = sc.nextInt();
